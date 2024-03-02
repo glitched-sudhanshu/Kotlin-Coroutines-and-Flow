1cf77c2e7fff7cd8a10db58beec191ba60c03230
glitched-sudhanshu <68722462+glitched-sudhanshu@users.noreply.github.com>
2024-03-02 16:53:40 +0530

    Update PerformSingleNetworkRequestViewModel.kt

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
@@ -1,13 +1,23 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class PerformSingleNetworkRequestViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performSingleNetworkRequest() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try{
+                val result = mockApi.getRecentAndroidVersions()
+                uiState.value = UiState.Success(result)
+            }catch (_: Exception){
+                uiState.value = UiState.Error("Something went wrong!")
+            }
+        }
     }
 }
