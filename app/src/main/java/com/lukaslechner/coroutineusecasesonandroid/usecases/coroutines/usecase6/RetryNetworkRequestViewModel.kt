845cd5fb6844ae08ac4cc4ab56c678e4d2c31aa6
glitched-sudhanshu <68722462+glitched-sudhanshu@users.noreply.github.com>
2024-03-05 01:26:39 +0530

    retry and backoff mechanism

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,48 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
-    private val api: MockApi = mockApi()
+    private val api: MockApi = mockApi(),
 ) : BaseViewModel<UiState>() {
-
     fun performNetworkRequest() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val versions =
+                    retry(2) {
+                        api.getRecentAndroidVersions()
+                    }
+                uiState.value = UiState.Success(versions)
+            } catch (_: Exception) {
+                uiState.value = UiState.Error("Something went wrong!")
+            }
+        }
     }
 
+    private suspend fun <T> retry(
+        noOfRetries: Int,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T,
+    ): T {
+        var currentDelay = initialDelayMillis
+        repeat(noOfRetries) {
+            try {
+                return block()
+            } catch (_: Exception) {
+                // log exception
+                Timber.e("Something went wrong!")
+            }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
+        }
+        return block()
+    }
 }
